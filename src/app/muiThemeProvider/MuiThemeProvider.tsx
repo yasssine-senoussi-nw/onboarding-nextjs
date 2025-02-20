fe3ef40330e6e7cb2c7d304a29c2dfeb22faa1a5
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-02-20 15:21:40 +0100

    fix(mui): use `PropsWithChildren` directly and deconstruct props inline
    Co-authored-by: Youssef Ben Sadik <159014180+youssef-bensadik-nw@users.noreply.github.com>

--- src/app/muiThemeProvider/MuiThemeProvider.tsx
+++ src/app/muiThemeProvider/MuiThemeProvider.tsx
@@ -3,7 +3,7 @@ import muiTheme from "./mui-theme";
 import { ThemeProvider } from "@emotion/react";
 import type { PropsWithChildren } from "react";
 
-function MuiThemeProvider(props: PropsWithChildren<{}>): JSX.Element {
+function MuiThemeProvider({ children }: PropsWithChildren): JSX.Element {
   const children = props.children ?? null;
   return <ThemeProvider theme={muiTheme}>{children}</ThemeProvider>;
 }
