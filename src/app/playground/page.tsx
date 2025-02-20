49bc13c5f002601c84f86da72ad2cb9bb38800a7
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-02-20 14:56:42 +0100

    fix(home): add items to the playground to silence unused components warning

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 import * as React from "react";
 
+import { Button } from "~components/elements/styled";
+import { ArrowRightIcon } from "~components/icons/arrow";
+import Banner3S from "~components/landing/banner3s";
+import { LandingEmotionButton } from "~components/landing/LandingEmotionButton";
+import { useTranslation } from "~i18n/useTranslation";
 import { useAppDispatch, useAppSelector } from "~store";
 import { decrement, increment, incrementByAmount, selectCount } from "~store/counter/slice";
 
 const PlaygroundPage: React.FC = () => {
+  const translate = useTranslation();
+
   const dispatch = useAppDispatch();
   const count = useAppSelector(selectCount);
   const [incrementAmount, setIncrementAmount] = React.useState<number>(0);
   return (
     <>
+      <LandingEmotionButton />
+      <Banner3S title="Playground" doc="https://react-redux.js.org/"></Banner3S>
+      <Button>{translate("common.styledButton")}</Button>
+      <ArrowRightIcon />
+
       <h2>
         The current number is
         {count}
