f14bfcc330f216cb2d95b11d8e1061c8efcec9e6
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-02-20 15:04:32 +0100

    fix(home): fix invalid image path for the logo at home page

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import theodoLogo from "~assets/theodo-logo.png";
 import { BackgroundContainer, Container, Logo, Subtitle, Title } from "~components/home/styles";
 import TranslateMessage from "~i18n/TranslateMessage";
 import txKeys from "~i18n/translations";
 
 import { Box, Stack } from "@mui/material";
+import theodoLogo from "public/assets/theodo.png";
 import type { FC } from "react";
 
 const Home: FC = () => {
