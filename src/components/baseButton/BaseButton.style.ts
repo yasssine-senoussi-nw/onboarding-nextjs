a151a297a4982428f87d7cc8f0f0819279291528
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-02-24 12:13:40 +0000

    chore(buttons/base-button): update style to center button and its icon

--- src/components/baseButton/BaseButton.style.ts
+++ src/components/baseButton/BaseButton.style.ts
@@ -6,4 +6,11 @@ export const BaseStyledButton = styled("button")`
   padding: 0.625rem 0.813rem;
   border-radius: 3.75rem;
   font-size: 1.125rem;
+  // https://github.com/yasssine-senoussi-nw/onboarding-nextjs/pull/5#discussion_r1967456288
+  // @youssef-bensadik-nw:
+  // needs a display property to show icon and text inline + a gap between them.
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
 `;
