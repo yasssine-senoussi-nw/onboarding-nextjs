9b38dff93251e38ec40d619c1dc0a6e0fcb8c3fb
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-02-24 12:07:56 +0000

    chore(buttons/base-button): use rem instead of hardcoded px

--- src/components/baseButton/BaseButton.style.ts
+++ src/components/baseButton/BaseButton.style.ts
@@ -1,9 +1,9 @@
 import { styled } from "@mui/material";
 
 export const BaseStyledButton = styled("button")`
-  width: 138px;
-  height: 56px;
-  padding: 10px 13px;
-  border-radius: 60px;
-  font-size: 18px;
+  width: 8.625rem;
+  height: 3.5rem;
+  padding: 0.625rem 0.813rem;
+  border-radius: 3.75rem;
+  font-size: 1.125rem;
 `;
