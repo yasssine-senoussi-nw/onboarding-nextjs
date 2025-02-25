06046158f2e7853df31a33e5ae7a5df1d82f123b
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-02-25 16:07:05 +0000

    feat: add blank signup/signin page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign in",
+};
+
+const SignInPage: React.FC = () => {
+  return <></>;
+};
+
+export default SignInPage;
