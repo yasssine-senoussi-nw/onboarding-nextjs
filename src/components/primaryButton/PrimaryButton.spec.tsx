3d29e11efe4687e2a9c1cf3786b9d7b3dfd781e7
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-02-25 11:59:47 +0000

    feat(primary-button-test): add test cases for primary button

---
+++ src/components/primaryButton/PrimaryButton.spec.tsx
@@ -0,0 +1,42 @@
+import { render, userEvent } from "~__test__/test-utils";
+import muiTheme from "~app/muiThemeProvider/mui-theme";
+import { PrimaryButton } from "~components/primaryButton";
+
+import { ThemeProvider } from "@emotion/react";
+
+describe("PrimaryButton component", () => {
+  const defaultProps = {
+    text: "Hello World",
+    onClick: jest.fn(),
+  };
+
+  let buttonElement: HTMLElement;
+
+  beforeEach(() => {
+    // reset the mock
+    jest.clearAllMocks();
+    const buttonComponent = render(
+      <ThemeProvider theme={muiTheme}>
+        <PrimaryButton text={defaultProps.text} onClick={defaultProps.onClick}></PrimaryButton>,
+      </ThemeProvider>,
+    );
+    buttonElement = buttonComponent.getByRole("button", { name: defaultProps.text });
+  });
+
+  it("should be in the DOM", () => {
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("should call the onClick handler when clicked", async () => {
+    const user = userEvent.setup();
+    await user.click(buttonElement);
+    expect(defaultProps.onClick).toHaveBeenCalled();
+  });
+
+  it("should change the background color when hovered", () => {
+    // user.hover(...), user.unhover(...) and fireEvent are broken
+    // jest can't simulate an user interaction directly
+    expect(buttonElement).toHaveStyle("background-color: white");
+    expect(buttonElement).toHaveStyle(`color: ${muiTheme.palette.secondary.main}`);
+  });
+});
