a87eeef05d93a1f20757d670456d938345ea6f99
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-02-26 17:33:24 +0000

    feat: add home's test by testing a mocked router

--- src/__test__/home.spec.tsx
+++ src/__test__/home.spec.tsx
@@ -1,31 +1,43 @@
-import MuiThemeProvider from "~app/muiThemeProvider/MuiThemeProvider";
+import { Renderer } from "~__test__/Renderer";
+import type { RenderResult } from "~__test__/test-utils";
+import { userEvent } from "~__test__/test-utils";
 import HomePage from "~app/page";
 
-import { render } from "./test-utils";
+const mockRouter = jest.fn();
+jest.mock("next/navigation", () => ({
+  useRouter: () => ({
+    push: mockRouter,
+  }),
+}));
 
-jest.mock("next/navigation");
-
-const bgImageUrl = "/assets/svg/background.svg";
+enum Constants {
+  bgImageUrl = "/assets/svg/background.svg",
+}
 
 describe("Landing page home component", () => {
   it("should be have one `div` with className 'landing'", () => {
-    const { container } = render(
-      <MuiThemeProvider>
-        <HomePage />
-      </MuiThemeProvider>,
-    );
+    const { container } = renderHomePage();
     const landing = container.getElementsByClassName("landing");
     expect(landing.length).toBe(1);
     expect(landing[0]?.tagName).toBe("DIV");
   });
 
-  it(`should be have a background image with the url '${bgImageUrl}'`, () => {
-    const { container } = render(
-      <MuiThemeProvider>
-        <HomePage />
-      </MuiThemeProvider>,
-    );
+  it(`should be have a background image with the url '${Constants.bgImageUrl}'`, () => {
+    const { container } = renderHomePage();
     const landing = container.getElementsByClassName("landing");
-    expect(landing[0]).toHaveStyle(`background-image: url(${bgImageUrl})`);
+    expect(landing[0]).toHaveStyle(`background-image: url(${Constants.bgImageUrl})`);
   });
+
+  it("should call mockRouter.push with 'signin' when the 'Sign In' button was pressed", async () => {
+    const homePage = renderHomePage();
+    // TODO: this should use txKeys and not a hardcoded value
+    const signInButton = homePage.getByRole("button", { name: "S'inscrire" });
+    expect(signInButton).not.toBeNull();
+    await userEvent.click(signInButton);
+    expect(mockRouter).toHaveBeenCalledWith("/signin");
+  });
+
+  function renderHomePage(): RenderResult {
+    return new Renderer(<HomePage />).withAllProviders().render();
+  }
 });
