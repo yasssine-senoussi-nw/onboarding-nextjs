da3b70febf6a313977bab214db144bca75d7a991
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-02-27 16:16:44 +0000

    feat: add basic tests for `TextInput`

---
+++ src/components/textInput/TextInput.spec.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Renderer } from "~__test__/Renderer";
+import TextInput from "~components/textInput/TextInput";
+
+describe("TextInput component", () => {
+  const defaultProps = {
+    type: "test",
+    label: "test",
+    value: "test",
+  };
+
+  let textInputElement: HTMLElement;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    const textInputRenderResult = new Renderer(<TextInput {...defaultProps} />).withAllProviders().render();
+    expect(textInputRenderResult).not.toBeNull();
+    textInputElement = textInputRenderResult.getByLabelText(defaultProps.label);
+  });
+
+  it("should be in the DOM", () => {
+    expect(textInputElement).toBeInTheDocument();
+  });
+
+  it("should render with the provided props", () => {
+    expect(textInputElement).toHaveAttribute("type", defaultProps.type);
+    expect(textInputElement).toHaveValue(defaultProps.value);
+  });
+});
